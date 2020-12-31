ce28132d33d1cda102023fb9d9f8f2c34638113b
kenorb <kenorb@users.noreply.github.com>
2020-12-31 22:25:25 +0000

    config: Updates period

--- config/EURUSD_M15.h
+++ config/EURUSD_M15.h
@@ -6,7 +6,7 @@
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_ADX_Params_M15 : Indi_ADX_Params {
   Indi_ADX_Params_M15() : Indi_ADX_Params(indi_adx_defaults, PERIOD_M15) {
-    period = 24;
+    period = 16;
     applied_price = (ENUM_APPLIED_PRICE)0;
     shift = 0;
   }
