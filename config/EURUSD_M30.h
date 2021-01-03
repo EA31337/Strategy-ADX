13380d29ca31cce4873cf274920ca7f129ac1252
kenorb <kenorb@users.noreply.github.com>
2021-01-03 21:43:48 +0000

    GHA: Adds new format of the optimization tests

--- config/EURUSD_M30.h
+++ config/EURUSD_M30.h
@@ -5,18 +5,17 @@
 
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_ADX_Params_M30 : Indi_ADX_Params {
-  Indi_ADX_Params_M30() : Indi_ADX_Params(indi_force_defaults, PERIOD_M30) {
+  Indi_ADX_Params_M30() : Indi_ADX_Params(indi_adx_defaults, PERIOD_M30) {
     applied_price = (ENUM_APPLIED_PRICE)3;
-    ma_method = 0;
     period = 24;
     shift = 0;
   }
-} indi_force_m30;
+} indi_adx_m30;
 
 // Defines strategy's parameter values for the given pair symbol and timeframe.
 struct Stg_ADX_Params_M30 : StgParams {
   // Struct constructor.
-  Stg_ADX_Params_M30() : StgParams(stg_force_defaults) {
+  Stg_ADX_Params_M30() : StgParams(stg_adx_defaults) {
     lot_size = 0;
     signal_open_method = 0;
     signal_open_filter = 1;
@@ -29,4 +28,4 @@ struct Stg_ADX_Params_M30 : StgParams {
     tick_filter_method = 1;
     max_spread = 0;
   }
-} stg_force_m30;
+} stg_adx_m30;
