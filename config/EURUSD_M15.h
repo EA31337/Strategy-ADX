2319c00eb100f0eb94b6ed0b49b0d15b51599c05
kenorb <kenorb@users.noreply.github.com>
2021-01-03 19:54:51 +0000

    Optimize Indicator params (pf=0.00; dd=0.00; tt=1)

--- config/EURUSD_M15.h
+++ config/EURUSD_M15.h
@@ -1,31 +1,32 @@
-/*
+/**
  * @file
  * Defines default strategy parameter values for the given timeframe.
  */
 
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_ADX_Params_M15 : Indi_ADX_Params {
-  Indi_ADX_Params_M15() : Indi_ADX_Params(indi_adx_defaults, PERIOD_M15) {
-    period = 16;
+  Indi_ADX_Params_M15() : Indi_ADX_Params(indi_force_defaults, PERIOD_M15) {
     applied_price = (ENUM_APPLIED_PRICE)2;
+    ma_method = 0;
+    period = 16;
     shift = 0;
   }
-} indi_adx_m15;
+} indi_force_m15;
 
 // Defines strategy's parameter values for the given pair symbol and timeframe.
 struct Stg_ADX_Params_M15 : StgParams {
   // Struct constructor.
-  Stg_ADX_Params_M15() : StgParams(stg_adx_defaults) {
+  Stg_ADX_Params_M15() : StgParams(stg_force_defaults) {
     lot_size = 0;
-    signal_open_method = -1;
-    signal_open_filter = 18;
+    signal_open_method = 0;
+    signal_open_filter = 1;
     signal_open_level = 100;
     signal_open_boost = 0;
-    signal_close_method = -3;
-    signal_close_level = 25;
+    signal_close_method = 0;
+    signal_close_level = 0;
     price_stop_method = 0;
-    price_stop_level = 2;
+    price_stop_level = 1;
     tick_filter_method = 1;
     max_spread = 0;
   }
-} stg_adx_m15;
+} stg_force_m15;
